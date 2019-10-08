929a6eeb376ca05a96a633c15326a781a11a3cb2
kozukaccd <t.kozuka.work@gmail.com>
2019-10-08 08:27:13 +0000

    本日のごはん

--- components/slider.vue
+++ components/slider.vue
@@ -1,29 +1,51 @@
 <template>
+<div>
   <carousel
-    :per-page="3"
-    :autoplay="true"
+    :perPageCustom="[[599,1],[959,2],[1263,3]]"
+    :autoplay="false"
     :loop="true"
-    :pagination-padding="1"
-    :autoplay-timeout="4000">
-    <slide>
-      <img src='https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg' />
+    :autoplay-timeout="4000"
+    :centerMode="true"
+    :navigation-enabled="true"
+    :navigation-prev-label="'〈'"
+    :navigation-next-label="'〉'">
+
+    <slide v-for="item in items" v-bind:key="item.id">
+        <v-card >
+            <v-card-title>{{item.title}}</v-card-title>
+            <v-card-text>{{item.text}}</v-card-text>
+                <v-card-actions>
+                    <v-btn :to="{name:'vote-entryNumber',params:{entryNumber:item.id}}" nuxt color="secondary" block >投票する</v-btn>
+                </v-card-actions>
+        </v-card>
     </slide>
-    <slide>
-      <img src='https://cdn.vuetifyjs.com/images/carousel/sky.jpg' />
-    </slide>
-    <slide>
-      <img src='https://cdn.vuetifyjs.com/images/carousel/bird.jpg' />
-    </slide>
-    <slide>
-      <img src='https://cdn.vuetifyjs.com/images/carousel/planet.jpg' />
-    </slide>
+
   </carousel>
+
+</div>
+
 </template>
 
 <script>
 import Carousel from 'vue-carousel/src/Carousel.vue'
 import Slide from 'vue-carousel/src/Slide.vue'
 export default {
+    data: function() {
+        return {
+          items:[
+            {id:1,title:"AAA", text:"aaaaaaaaaaa"},
+            {id:2,title:"BBB", text:"bbbbbbbbbbb"},
+            {id:3,title:"CCC", text:"ccccccccccc"},
+            {id:4,title:"DDD", text:"ddddddddddd"},
+            {id:5,title:"EEE", text:"eeeeeeeeeee"},
+            {id:6,title:"FFF", text:"fffffffffff"},
+            {id:7,title:"GGG", text:"ggggggggggg"},
+            {id:8,title:"HHH", text:"hhhhhhhhhhh"},
+            {id:9,title:"III", text:"iiiiiiiiiii"},
+        ]
+
+        }},
+
   components: {
     Carousel,
     Slide
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
     <v-container>
-        <v-card-title>新規会員登録フォーム(3分で完了)</v-card-title>
-        <nuxt-link to="/vote/1234556">YEAH come on baby</nuxt-link>
-        <nuxt-link to="/adminpage">hereherehere</nuxt-link>
+       <v-layout>
+        <v-row>
+            <v-col cols="4">
+                <v-card-title>新規会員登録フォーム(3分で完了)</v-card-title>
+                <nuxt-link to="/vote/1234556">個別投票ページ</nuxt-link>
+                <nuxt-link to="/entries">参加作品一覧</nuxt-link>
+            </v-col>
+        </v-row>
+       </v-layout>
     </v-container>
 </template>
 
